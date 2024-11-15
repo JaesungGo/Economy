507e583ed132968c5db619df4cdab503dcc0552c
Jeon Minju <odeto1310@gmail.com>
2024-11-15 21:04:59 +0900

    Design : 템플릿 원본

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,65 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Tables from "../views/Tables.vue";
+import Billing from "../views/Billing.vue";
+import VirtualReality from "../views/VirtualReality.vue";
+import RTL from "../views/Rtl.vue";
+import Profile from "../views/Profile.vue";
+import Signup from "../views/Signup.vue";
+import Signin from "../views/Signin.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "/",
+    redirect: "/dashboard-default",
+  },
+  {
+    path: "/dashboard-default",
+    name: "Dashboard",
+    component: Dashboard,
+  },
+  {
+    path: "/tables",
+    name: "Tables",
+    component: Tables,
+  },
+  {
+    path: "/billing",
+    name: "Billing",
+    component: Billing,
+  },
+  {
+    path: "/virtual-reality",
+    name: "Virtual Reality",
+    component: VirtualReality,
+  },
+  {
+    path: "/rtl-page",
+    name: "RTL",
+    component: RTL,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+  },
+  {
+    path: "/signin",
+    name: "Signin",
+    component: Signin,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  linkActiveClass: "active",
+});
 
-export default router
+export default router;
