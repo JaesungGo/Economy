15cdae4404d8392c53ae1687f3f8e085ba3a9fb9
Jeon Minju <odeto1310@gmail.com>
2024-11-19 22:01:36 +0900

    Feat : memberApi.js - login, logout 추가

--- frontend/src/api/memberApi.js
+++ frontend/src/api/memberApi.js
@@ -1,5 +1,4 @@
 import api from '@/api';
-import axios from 'axios';
 
 // const api = axios.create({
 //   baseURL: '/api/member', // 백엔드 API 기본 URL
@@ -13,8 +12,42 @@ const headers = { 'Content-Type': 'application/json' };
 
 export default {
     async create(member) {
-        const { data } = await.api.post(`${BASE_URL}/join`, member, headers);
+        const { data } = await api.post(`${BASE_URL}/join`, member, headers);
         return data;
-    }
-}
+    },
 
+    //로그인
+    async login(loginDTO) {
+        const { data } = await api.post(`${BASE_URL}/login`, loginDTO, headers);
+        console.log('POST MemberLogin :', data);
+        return data;
+    },
+
+    //로그아웃
+    async logout() {
+        const { data } = await api.post(`${BASE_URL}/logout`, {}, headers);
+        console.log('POST Logout : ', data);
+        return data;
+    },
+
+    // 로그인 확인
+    async checkLogin() {
+        const { data } = await api.get(`${BASE_URL}/check-login`, headers);
+        console.log('GET CheckLogin :', data);
+        return data; // 현재 로그인 상태 반환
+    },
+
+    // 회원 정보 조회
+    async getMember() {
+        const { data } = await api.get(`${BASE_URL}/`, headers);
+        console.log('Get Member GET:', data);
+        return data; // 회원 정보 반환
+    },
+
+    // 회원탈퇴
+    async deleteMember(memberNo) {
+        const { data } = await api.delete(`${BASE_URL}/${memberNo}`, headers);
+        console.log('DELETE DeleteMember :', data);
+        return data; // 회원탈퇴 성공 여부 반환
+    },
+};
